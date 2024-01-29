8f00ef89ba143c58eab6df62a6e30fc4ae7f0f7a
Rofael-Alfons <rofa.alfons7@gmail.com>
2024-01-30 00:15:23 +0200

    first commit

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,115 +1,186 @@
-import React, {useEffect, useState} from "react";
-import {useNavigate} from "react-router-dom";
-import {getUser} from "../user";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { getUser } from "../user";
+import { AuthenticationDetails, CognitoUser } from "amazon-cognito-identity-js";
+import UserPool, { verifier } from "../UserPool";
+import { CognitoJwtVerifier } from "aws-jwt-verify";
 
 export default function Login() {
-    const navigate = useNavigate();
-    const [params, setParams] = useState({});
-    const [errors, setErrors] = useState({});
-    const handleChange = (e) => {
-        const {name, value} = e.target;
+  const navigate = useNavigate();
+  const [params, setParams] = useState({});
+  const [errors, setErrors] = useState({});
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-        setParams(prev => {
-            return {
-                ...prev,
-                [name]: value
-            }
-        });
+    setParams((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
 
-        setError(name, "");
-        setError("not_registered", "");
-        if (name === "password") {
-            setError("incorrect_password", "");
-        }
+    setError(name, "");
+    setError("not_registered", "");
+    if (name === "password") {
+      setError("incorrect_password", "");
     }
+  };
 
-    const user = getUser();
+  const user = getUser();
 
-    useEffect(() => {
-        if (user) {
-            navigate("/events");
-        }
-    }, []);
+  useEffect(() => {
+    // Example: Verify JWT received after successful login
+    if (user && user.signInUserSession) {
+      const accessToken = user.signInUserSession.getAccessToken().getJwtToken();
 
-    const setError = (name, value) => {
-        setErrors(prev => {
-            return {
-                ...prev,
-                [name]: value
-            }
+      verifier
+        .verify(accessToken)
+        .then((res) => {
+          navigate("/events");
+          console.log(res);
         })
+        .catch((err) => {
+          console.log(err);
+        });
     }
 
-    const login = () => {
-        const {email, password} = params;
+    // if (user) {
+    //   navigate("/events");
+    // }
+  }, [user]); // Ensure to add user as a dependency
 
-        if (!email) {
-            setError("email", "email is required");
-        }
+  const setError = (name, value) => {
+    setErrors((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
 
-        if (!password) {
-            setError("password", "password is required");
-        }
+  // const login = () => {
+  //     const {email, password} = params;
 
-        if (email && password) {
-            const users = [];
+  //     if (!email) {
+  //         setError("email", "email is required");
+  //     }
 
-            users.push(...JSON.parse(localStorage.getItem("users")) || []);
+  //     if (!password) {
+  //         setError("password", "password is required");
+  //     }
 
-            const exist = users.find((u) => u?.email === params.email);
-            if (exist) {
-                if (exist.password !== params.password) {
-                    setError("incorrect_password", "Incorrect Password");
-                } else {
-                    exist.login = true;
-                    localStorage.setItem("users", JSON.stringify(users));
-                    navigate("/events");
-                    setParams({});
-                }
+  //     if (email && password) {
+  //         const users = [];
 
-            } else {
-                setError("not_registered", "User not Registered");
-            }
-        }
-    }
+  //         users.push(...JSON.parse(localStorage.getItem("users")) || []);
 
-    return (
-        <div className="card">
-            <div className="form">
-                <div className="title">
-                    <p>Login</p>
-                </div>
-                {
-                    !!errors.not_registered && <span className="mandatory">{errors.not_registered}</span>
-                }
-                <div className="space"></div>
-                <div>
-                    <label>Email</label>
-                    <input type="text" name="email" value={params.email || ""} onChange={handleChange}/>
-                    {
-                        !!errors.email && <span className="mandatory">{errors.email}</span>
-                    }
-                </div>
-                <div className="space"></div>
-                <div>
-                    <label>Password</label>
-                    <input type="password" name="password" value={params.password || ""} onChange={handleChange}/>
-                    {
-                        !!errors.password && <span className="mandatory">{errors.password}</span>
-                    }
-                    {
-                        !!errors.incorrect_password && <span className="mandatory">{errors.incorrect_password}</span>
-                    }
-                </div>
-                <div className="space"></div>
-                <div>
-                    <button onClick={() => login()}>Login</button>
-                </div>
-                <div className="space"></div>
-                <div>
-                    <span>Not registered? <span className="account" onClick={() => navigate("/register")}>Create an account</span></span>
-                </div>
-            </div>
+  //         const exist = users.find((u) => u?.email === params.email);
+  //         if (exist) {
+  //             if (exist.password !== params.password) {
+  //                 setError("incorrect_password", "Incorrect Password");
+  //             } else {
+  //                 exist.login = true;
+  //                 localStorage.setItem("users", JSON.stringify(users));
+  //                 navigate("/events");
+  //                 setParams({});
+  //             }
+
+  //         } else {
+  //             setError("not_registered", "User not Registered");
+  //         }
+  //     }
+  // }
+
+  const login = (e) => {
+    e.preventDefault();
+    const user = new CognitoUser({
+      Username: params.email,
+      Pool: UserPool,
+    });
+
+    const authDetails = new AuthenticationDetails({
+      Username: params.email,
+      Password: params.password,
+    });
+
+    user.authenticateUser(authDetails, {
+      onSuccess: (session) => {
+        const accessToken = session.getAccessToken().getJwtToken();
+        // Save the token securely (e.g., in local storage, secure cookie, etc.)
+        localStorage.setItem("accessToken", accessToken);
+
+        // Verify the JWT
+        verifier
+          .verify(accessToken)
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+
+        // Redirect or perform other actions as needed
+        navigate("/events");
+      },
+      onFailure: (data) => {
+        console.error(data);
+      },
+      newPasswordRequired: (data) => {
+        console.log(data);
+      },
+    });
+  };
+
+  return (
+    <div className="card">
+      <form className="form" onSubmit={login}>
+        <div className="title">
+          <p>Login</p>
         </div>
-    )
+        {!!errors.not_registered && (
+          <span className="mandatory">{errors.not_registered}</span>
+        )}
+        <div className="space"></div>
+        <div>
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            value={params.email || ""}
+            onChange={handleChange}
+          />
+          {!!errors.email && <span className="mandatory">{errors.email}</span>}
+        </div>
+        <div className="space"></div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={params.password || ""}
+            onChange={handleChange}
+          />
+          {!!errors.password && (
+            <span className="mandatory">{errors.password}</span>
+          )}
+          {!!errors.incorrect_password && (
+            <span className="mandatory">{errors.incorrect_password}</span>
+          )}
+        </div>
+        <div className="space"></div>
+        <div>
+          <button>Login</button>
+        </div>
+        <div className="space"></div>
+        <div>
+          <span>
+            Not registered?{" "}
+            <span className="account" onClick={() => navigate("/register")}>
+              Create an account
+            </span>
+          </span>
+        </div>
+      </form>
+    </div>
+  );
 }
--- src/UserPool.js
+++ src/UserPool.js
@@ -1,8 +1,17 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
+import { CognitoJwtVerifier } from "aws-jwt-verify";
 
 const poolData = {
-  UserPoolId: "eu-north-1_ScIPHgUJm",
-  ClientId: "6chh9n88t3qi51qeg71vr36eqb",
+  UserPoolId: "eu-north-1_gkqAD67fu",
+  ClientId: "5echiavgqu51r1dvbp9t6jk752",
 };
 
+// Verifier that expects valid access tokens:
+const verifier = CognitoJwtVerifier.create({
+  userPoolId: "eu-north-1_gkqAD67fu",
+  tokenUse: "access",
+  clientId: "5echiavgqu51r1dvbp9t6jk752",
+});
+
+export { verifier };
 export default new CognitoUserPool(poolData);
