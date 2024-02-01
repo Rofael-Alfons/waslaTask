169a71da3ff8d65bc463080559e4b7b2e9f5a0b8
Rofael-Alfons <rofa.alfons7@gmail.com>
2024-02-01 16:57:15 +0200

    some modification

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,19 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getUser } from "../user";
+import { CognitoJwtVerifier } from "aws-jwt-verify";
+
+const LOCAL_STORAGE_KEY_ACCESS_TOKEN = "accessToken";
 
 export default function Profile({ children }) {
   const navigate = useNavigate();
   const user = getUser();
+  const [profileName, setProfileName] = useState("");
+
+  useEffect(() => {
+    // Fetch and display profile name from the AWS Cognito token
+    const fetchProfileName = async () => {
+      if (user && user.signInUserSession) {
+        const accessToken = user.signInUserSession
+          .getAccessToken()
+          .getJwtToken();
+
+        try {
+          const decodedToken = await CognitoJwtVerifier.verify(accessToken);
+          const customProfileName = decodedToken["custom:profileName"];
+
+          if (customProfileName) {
+            setProfileName(customProfileName);
+            console.log(customProfileName);
+          }
+        } catch (error) {
+          console.error("Token verification failed:", error);
+        }
+      }
+    };
+
+    fetchProfileName();
+  }, [user]);
   const logOut = () => {
-    const users = [];
-
-    users.push(...(JSON.parse(localStorage.getItem("users")) || []));
-
-    const user = users.find((u) => u.login);
     if (user) {
-      user.login = undefined;
-      localStorage.setItem("users", JSON.stringify(users));
+      // Clear user session and related information
+      user.signOut();
+
+      // Clear stored access token
+      localStorage.removeItem(LOCAL_STORAGE_KEY_ACCESS_TOKEN);
+
+      // Redirect to the login page or another appropriate location
       navigate("/");
     }
   };
@@ -23,7 +52,9 @@ export default function Profile({ children }) {
     >
       <div style={{ textAlign: "center" }}>
         <p>Profile</p>
-        <p style={{ fontSize: "20px" }}>Rofa</p>
+        <p style={{ fontSize: "20px" }}>
+          {profileName || "Default Profile Name"}
+        </p>
         <div style={{ marginTop: "10px" }}>
           {children}
           <span>&nbsp;</span>
